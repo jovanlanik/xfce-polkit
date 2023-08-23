7f86ec1cca8734a610687e0a11408432b8377180
Jovan Lanik <jox969@gmail.com>
2023-08-23 14:53:44 +0200

    create polkit subject not based on session to avoid logind

--- src/xfce-polkit.c
+++ src/xfce-polkit.c
@@ -4,18 +4,25 @@
 int main(int argc, char *argv[])
 {
 	PolkitAgentListener *listener;
-	PolkitSubject* session;
+	PolkitSubject* subject;
 	GError* err = NULL;
 	int rc = 0;
 
 	gtk_init(&argc, &argv);
 
 	listener = xfce_polkit_listener_new();
-	session = polkit_unix_session_new_for_process_sync(getpid(), NULL, NULL);
+	pid_t pid_of_caller;
+	pid_of_caller = getppid();
+	subject = polkit_unix_process_new_for_owner(pid_of_caller, 0, getuid ());
+
+	g_assert(subject != NULL);
+	g_assert(polkit_unix_process_get_pid(POLKIT_UNIX_PROCESS (subject)) == pid_of_caller);
+	g_assert(polkit_unix_process_get_uid(POLKIT_UNIX_PROCESS (subject)) >= 0);
+	g_assert(polkit_unix_process_get_start_time(POLKIT_UNIX_PROCESS (subject)) > 0);
 
 	if(!polkit_agent_listener_register(listener,
 					   POLKIT_AGENT_REGISTER_FLAGS_NONE,
-					   session, NULL, NULL, &err)) {
+					   subject, NULL, NULL, &err)) {
 		xfce_dialog_show_error(NULL, err, "XFCE PolicyKit Agent");
 		rc = 1;
 	} else {
@@ -23,6 +30,6 @@ int main(int argc, char *argv[])
 	}
 
 	g_object_unref(listener);
-	g_object_unref(session);
+	g_object_unref(subject);
 	return rc;
 }
